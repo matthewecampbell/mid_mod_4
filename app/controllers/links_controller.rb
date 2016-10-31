441a715d2b6fe0da45a86e589ae2e21171432193
Matthew Campbell <ematthewcampbell@gmail.com>
2016-10-31 12:17:25 -0600

    got rid of show, 100 percent covered

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,16 +4,13 @@ class LinksController < ApplicationController
     redirect_to root_path if !current_user
   end
 
-  def show
-    @link = Link.find(params[:id])
-  end
 
   def create
     @link = Link.new(link_params)
     @link.user_id = current_user.id if current_user
     if @link.save
       flash[:success] = "Link Created."
-      redirect_to link_path(@link)
+      render :index
     else
       flash.now[:warning] = "Please enter a valid URL."
       render :index
